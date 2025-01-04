45366fd742ad3554f5136fc9a8c44e627bdbdd6f
albertoakel <alberto.akel@gmail.com>
2025-01-04 12:16:14 -0300

    up

--- main.py
+++ main.py
@@ -8,43 +8,43 @@ def seir(y, t, n, beta, gamma, delta):
     drdt = gamma * i
     return dsdt, dedt, didt, drdt
 
-def SEIRD(y, t, N, beta, gamma, delta, alpha, rho):
-    S, E, I, R, D = y
-    dSdt = -beta * S * I / N
-    dEdt = beta * S * I / N - delta * E
-    dIdt = delta * E - (1 - alpha) * gamma * I - alpha * rho * I
-    dRdt = (1 - alpha) * gamma * I
-    dDdt = alpha * rho * I
-    return dSdt, dEdt, dIdt, dRdt, dDdt
+def seird(y, t, n, beta, gamma, delta, alpha, rho):
+    s, e, i, r, d = y
+    dsdt = -beta * s * i / n
+    dedt = beta * s * i / n - delta * e
+    didt = delta * e - (1 - alpha) * gamma * i - alpha * rho * i
+    drdt = (1 - alpha) * gamma * i
+    dddt = alpha * rho * i
+    return dsdt, dedt, didt, drdt, dddt
 
-def SEIRD2(y, t, N, beta, gamma, delta, alpha, rho):
-    S, E, I, R, D = y
-    dSdt = -beta(t) * S * I / N
-    dEdt = beta(t) * S * I / N - delta * E
-    dIdt = delta * E - (1 - alpha) * gamma * I - alpha * rho * I
-    dRdt = (1 - alpha) * gamma * I
-    dDdt = alpha * rho * I
-    return dSdt, dEdt, dIdt, dRdt, dDdt
+# def seird2(y, t, n, beta, gamma, delta, alpha, rho):
+#     s, e, i, r, d = y
+#     dsdt = -beta(t) * s * i / n
+#     dedt = beta(t) * s * i / n - delta * e
+#     didt = delta * e - (1 - alpha) * gamma * i - alpha * rho * i
+#     drdt = (1 - alpha) * gamma * i
+#     dddt = alpha * rho * i
+#     return dsdt, dedt, didt, drdt, dddt
+#
+# def r_0(t):
+#     return 5.0 if t < t else 0.8
+# def beta(t):
+#     return r_0(t) * gamma
 
-def R_0(t):
-    return 5.0 if t < TL else 0.8
-def beta(t):
-    return R_0(t) * gamma
-
-def deriv(y, t, N, beta, gamma, delta, alpha_opt, rho):
-    S, E, I, R, D = y
-    def alpha(t):
-        return s * I/N + alpha_opt
-
-    dSdt = -beta(t) * S * I / N
-    dEdt = beta(t) * S * I / N - delta * E
-    dIdt = delta * E - (1 - alpha(t)) * gamma * I - alpha(t) * rho * I
-    dRdt = (1 - alpha(t)) * gamma * I
-    dDdt = alpha(t) * rho * I
-    return dSdt, dEdt, dIdt, dRdt, dDdt
-
-def logistic_R_0(t):
-    return (R_ini-R_end) / (1 + np.exp(-k*(-t+xm))) + R_end
+# def deriv(y, t, n, beta, gamma, delta, alpha_opt, rho):
+#     s, e, i, r, d = y
+#     def alpha(t):
+#         return s * i/n + alpha_opt
+#
+#     dsdt = -beta(t) * s * i / n
+#     dedt = beta(t) * s * i / n - delta * e
+#     didt = delta * e - (1 - alpha(t)) * gamma * i - alpha(t) * rho * i
+#     drdt = (1 - alpha(t)) * gamma * i
+#     dddt = alpha(t) * rho * i
+#     return dsdt, dedt, didt, drdt, dddt
+#
+# def logistic_r_0(t):
+#     return (r_ini-r_end) / (1 + np.exp(-k*(-t+xm))) + r_end
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -53,25 +53,25 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
  #1) seir Model
-    n = 1000000
-    d = 4.0  # número de dias que uma pessoa propaga a doença
-    gamma = 1.0 / d
-    delta = 1.0 / 5.0  # (periodo de incubação 5 dias)
-    r_0 = 5.0  # também definido como beta/gama, é o valor total de pessoas infectadas
-            # por uma unica pessoa.
-
-    beta = r_0 * gamma
-    s0, e0, i0, r0 = n - 1, 1, 0, 0
-    t = np.linspace(0, 99, 100)
-    y0 = s0, e0, i0, r0
-
-    out = odeint(seir, y0, t, args=(n, beta, gamma, delta))
-    s, e, i, r = out.T
-
-    plt.plot(t, s, label='S')
-    plt.plot(t, e, label='E')
-    plt.plot(t, i, label='I')
-    plt.plot(t, r, label='R')
-    plt.legend()
-    plt.show()
+    # n = 1000000
+    # d = 4.0  # número de dias que uma pessoa propaga a doença
+    # gamma = 1.0 / d
+    # delta = 1.0 / 5.0  # (periodo de incubação 5 dias)
+    # r_0 = 5.0  # também definido como beta/gama, é o valor total de pessoas infectadas
+    #         # por uma unica pessoa.
+    #
+    # beta = r_0 * gamma
+    # s0, e0, i0, r0 = n - 1, 1, 0, 0
+    # t = np.linspace(0, 99, 100)
+    # y0 = s0, e0, i0, r0
+    #
+    # out = odeint(seir, y0, t, args=(n, beta, gamma, delta))
+    # s, e, i, r = out.T
+    #
+    # plt.plot(t, s, label='S')
+    # plt.plot(t, e, label='E')
+    # plt.plot(t, i, label='I')
+    # plt.plot(t, r, label='R')
+    # plt.legend()
+    # plt.show()
 
